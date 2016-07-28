b225577bde2ea6922a251ed8fb14ae0625186dbd
cpsa3 <1090303279@qq.com>
2016-07-28 20:41:12 +0800

    修复ios照片旋转问题

--- src/ajax-file-upload-without-jq.js
+++ src/ajax-file-upload-without-jq.js
@@ -1,7 +1,180 @@
 /**
- * require Promise.js
+ * require Promise.js,exif.js
  */
-(function () {
+(function (window) {
+    window.URL = window.URL || window.webkitURL;
+
+    var UA = (function (userAgent) {
+        var ISOldIOS = /OS (\d)_.* like Mac OS X/g.exec(userAgent),
+            isOldAndroid = /Android (\d.*?);/g.exec(userAgent) || /Android\/(\d.*?) /g.exec(userAgent);
+
+        // 判断设备是否是IOS7以下
+        // 判断设备是否是android4.5以下
+        // 判断是否iOS
+        // 判断是否android
+        // 判断是否QQ浏览器
+        return {
+            oldIOS: ISOldIOS ? +ISOldIOS.pop() < 8 : false,
+            oldAndroid: isOldAndroid ? +isOldAndroid.pop().substr(0, 3) < 4.5 : false,
+            iOS: /\(i[^;]+;( U;)? CPU.+Mac OS X/.test(userAgent),
+            android: /Android/g.test(userAgent),
+            mQQBrowser: /MQQBrowser/g.test(userAgent)
+        }
+    })(navigator.userAgent);
+
+    /**
+     * 转换成formdata
+     * @param dataURI
+     * @returns {*}
+     *
+     * @source http://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata
+     */
+    function dataURItoBlob(dataURI) {
+        // convert base64/URLEncoded data component to raw binary data held in a string
+        var byteString;
+        if (dataURI.split(',')[0].indexOf('base64') >= 0)
+            byteString = atob(dataURI.split(',')[1]);
+        else
+            byteString = unescape(dataURI.split(',')[1]);
+
+        // separate out the mime component
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+        // write the bytes of the string to a typed array
+        var ia = new Uint8Array(byteString.length);
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+
+        return new Blob([ia], { type: mimeString });
+    }
+
+    // 使用canvas将file转成base64,并fix ios图片旋转问题
+    function _getBase64(file) {
+
+        var that = this;
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
+        var image = new Image();
+        var orientation;
+
+        if (!document.createElement('canvas').getContext) {
+            throw new Error('浏览器不支持canvas');
+        }
+
+        EXIF.getData(file, function () {
+            orientation = EXIF.getTag(this, "Orientation");
+        });
+
+
+        return new Promise(function (resolve, reject) {
+
+            image.onload = function () {
+
+                var resize = _getResize(this, orientation);
+
+                canvas.width = resize.width;
+                canvas.height = resize.height;
+
+                var base64 = null;
+
+                // 调整为正确方向
+                switch (orientation) {
+                    case 3:
+                        ctx.rotate(180 * Math.PI / 180);
+                        ctx.drawImage(this, -canvas.width, -canvas.height, canvas.width, canvas.height);
+                        break;
+                    case 6:
+                        ctx.rotate(90 * Math.PI / 180);
+                        ctx.drawImage(this, 0, -canvas.width, canvas.height, canvas.width);
+                        break;
+                    case 8:
+                        ctx.rotate(270 * Math.PI / 180);
+                        ctx.drawImage(this, -canvas.height, 0, canvas.height, canvas.width);
+                        break;
+
+                    case 2:
+                        ctx.translate(canvas.width, 0);
+                        ctx.scale(-1, 1);
+                        ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+                        break;
+                    case 4:
+                        ctx.translate(canvas.width, 0);
+                        ctx.scale(-1, 1);
+                        ctx.rotate(180 * Math.PI / 180);
+                        ctx.drawImage(this, -canvas.width, -canvas.height, canvas.width, canvas.height);
+                        break;
+                    case 5:
+                        ctx.translate(canvas.width, 0);
+                        ctx.scale(-1, 1);
+                        ctx.rotate(90 * Math.PI / 180);
+                        ctx.drawImage(this, 0, -canvas.width, canvas.height, canvas.width);
+                        break;
+                    case 7:
+                        ctx.translate(canvas.width, 0);
+                        ctx.scale(-1, 1);
+                        ctx.rotate(270 * Math.PI / 180);
+                        ctx.drawImage(this, -canvas.height, 0, canvas.height, canvas.width);
+                        break;
+
+                    default:
+                        ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+                }
+
+                // createBase64
+                base64 = _createBase64(canvas, ctx);
+
+                resolve(dataURItoBlob(base64));
+            };
+
+            image.src = URL.createObjectURL(file);
+        });
+    }
+
+    function _getOrientation(file) {
+        return new Promise(function (resolve, reject) {
+            EXIF.getData(file, function () {
+                orientation = EXIF.getTag(this, "Orientation");
+                resolve(orientation);
+            });
+        });
+    }
+
+    function _createBase64(canvas, ctx) {
+        var base64 = null;
+        if (UA.oldAndroid || UA.mQQBrowser || !navigator.userAgent) {
+            var encoder = new JPEGEncoder();
+            base64 = encoder.encode(ctx.getImageData(0, 0, canvas.width, canvas.height), 80);
+        }
+        else {
+            base64 = canvas.toDataURL('image/jpeg', 0.8);
+        }
+        return base64;
+    }
+
+
+    function _getResize(img, orientation) {
+        var ret = {
+            width: img.width,
+            height: img.height
+        };
+
+        // 修正图片旋转90度情况
+        if ("5678".indexOf(orientation) > -1) {
+            ret.width = img.height;
+            ret.height = img.width;
+        }
+
+        // 超过这个值base64无法生成，在IOS上
+        while (ret.width >= 3264 || ret.height >= 2448) {
+            ret.width *= 0.8;
+            ret.height *= 0.8;
+        }
+
+        return ret;
+    }
+
+
     window.ajaxUpload = function (fileSelecter, opts, checkFn, progressFn) {
         var that = this;
 
@@ -31,6 +204,38 @@
         that.fileInput = fileSelecter;
         that.checkFn = checkFn;
         that.progressFn = progressFn;
+        that.canvas = document.createElement("canvas");
+    };
+
+    ajaxUpload.prototype._verify = function () {
+        var that = this;
+        var files = that.fileInput.files;
+
+        if (files.length < 1) {
+            return {
+                status: 0,
+                msg: 'The number of files cannot be empty.'
+            };
+        }
+
+        if (typeof that.checkFn === "function" && !that.checkFn(files[0])) {
+            return {
+                status: 0,
+                msg: 'checkFn error'
+            };
+        }
+
+        if (that.defaults.maxFileSize && files[0].size > that.defaults.maxFileSize) {
+            return {
+                status: 0,
+                msg: 'size over limit'
+            };
+        }
+
+        return {
+            status: 1,
+            msg: ''
+        };
     };
 
     ajaxUpload.prototype.upload = function () {
@@ -41,18 +246,11 @@
         return new Promise(function (resolve, reject) {
 
             // append files
-            for (var i = 0, len = files.length; i < len; i++) {
-                if (typeof that.checkFn === "function" && !that.checkFn(files[i])) {
-                    reject('checkFn error');
-                    return;
-                }
+            var verify_res = that._verify();
 
-                if (that.defaults.maxFileSize && files[i].size > that.defaults.maxFileSize) {
-                    reject('size over limit');
-                    return;
-                }
-
-                formData.append(files[i].name, files[i]);
+            if (verify_res.status !== 1) {
+                reject(verify_res.msg);
+                return;
             }
 
             //append post data
@@ -90,7 +288,14 @@
             };
 
             xhr.open('POST', that.defaults.action, true);
-            xhr.send(formData);
+
+            // file to base64 and fix ios picture orientation
+            _getBase64(files[0]).then(function (value) {
+                //document.getElementById("base64-container").innerText = value;
+                formData.append(files[0].name, value);
+
+                xhr.send(formData);
+            });
         });
     };
-})();
+})(window);
